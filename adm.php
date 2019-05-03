7ff4d14e141ff9906a5fb3dee06a5d35dce050dd
Dakshansh <47587379+Dakshansh@users.noreply.github.com>
2019-05-03 13:03:36 +0530

    Add files via upload

---
+++ adm.php
@@ -0,0 +1,18 @@
+<?php
+include('database.php');
+?>
+
+
+<html>
+<body>
+
+
+<form action="validate.php" method="POST">
+LOGIN:<input type="text" name="admin"/><br/>
+Password:<input type="password" name="pass"/><br/>
+<input type="submit" name="submit" value="submit">
+
+</form>
+
+</body>
+</html>
